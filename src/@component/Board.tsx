528abe26537ca6d2870cad84d95906c158e0a266
nana.like <nana.like@kakaoenterprise.com>
2022-06-17 14:59:54 +0900

    6.11-6.13 Refs

--- src/@component/Board.tsx
+++ src/@component/Board.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Droppable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 import DraggableCard from './DraggableCard';
@@ -9,25 +9,46 @@ interface BoardProps {
 }
 
 const Board = ({ boardId, toDos }: BoardProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onClick = () => {
+    inputRef.current?.focus();
+  };
+
   return (
-    <Droppable droppableId={boardId}>
-      {(provided) => (
-        <Wrapper ref={provided.innerRef} {...provided.droppableProps}>
-          {toDos.map((toDo, index) => (
-            <DraggableCard index={index} key={toDo} toDo={toDo} />
-          ))}
-          {provided.placeholder}
-        </Wrapper>
-      )}
-    </Droppable>
+    <>
+      <div>
+        <input ref={inputRef} />
+        <button onClick={onClick}>Click me</button>
+      </div>
+      <Droppable droppableId={boardId}>
+        {(provided, snapshot) => (
+          <Wrapper
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            isDraggingOver={snapshot.isDraggingOver}
+            isDraggingFromThis={!!snapshot.draggingFromThisWith}
+          >
+            {toDos.map((toDo, index) => (
+              <DraggableCard index={index} key={toDo} toDo={toDo} />
+            ))}
+            {provided.placeholder}
+          </Wrapper>
+        )}
+      </Droppable>
+    </>
   );
 };
 
 export default Board;
 
-const Wrapper = styled.ul`
+const Wrapper = styled.ul<{ isDraggingOver: boolean; isDraggingFromThis: boolean }>`
   padding: 2rem 1rem;
   padding-top: 3rem;
-  background-color: ${(props) => props.theme.boardColor};
+  background-color: ${(props) =>
+    props.isDraggingOver
+      ? 'lightpink'
+      : props.isDraggingFromThis
+      ? 'lightgreen'
+      : props.theme.boardColor};
   border-radius: 0.5rem;
 `;
--- src/@component/DraggableCard.tsx
+++ src/@component/DraggableCard.tsx
@@ -11,11 +11,12 @@ const DraggableCard = ({ toDo, index }: DragabbleCardProps) => {
   console.log(`${toDo} has been rendered.`);
   return (
     <Draggable draggableId={toDo} index={index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <Card
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
+          isDragging={snapshot.isDragging}
         >
           {toDo}
         </Card>
@@ -26,14 +27,10 @@ const DraggableCard = ({ toDo, index }: DragabbleCardProps) => {
 
 export default React.memo(DraggableCard);
 
-const Card = styled.li`
+const Card = styled.li<{ isDragging: boolean }>`
   border-radius: 0.5rem;
   margin-bottom: 0.5rem;
   padding: 1rem;
-  background-color: ${(props) => props.theme.cardColor};
-
-  &:hover,
-  &:active {
-    color: red;
-  }
+  background-color: ${(props) => (props.isDragging ? '#999' : props.theme.cardColor)};
+  color: ${(props) => (props.isDragging ? '#fff' : '#222')};
 `;
